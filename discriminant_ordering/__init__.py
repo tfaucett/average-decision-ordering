7acb63febbb58b24478d3866a226d197a35f2ad8
Taylor Faucett <taylor@hep-dhcp178.ps.uci.edu>
2018-03-27 16:14:13 -0700

    Fixed error in bootstrap

--- discriminant_ordering/__init__.py
+++ discriminant_ordering/__init__.py
@@ -34,34 +34,39 @@ def calc_DO(fx, gx, target, n_data):
             heavi_sum = heavi_side + heavi_sum
     return 2*(np.abs((float(1.0/len(fx0)) * float(1.0/len(gx0)) * heavi_sum) - 0.5))
 
-''' Ordering Discriminant '''
-def DO(fx, gx, target, n_data, stats=False):
+
+def boot_strap(fx, gx, target, n_data, boot_loops):
+    # Calculate uncertainty using bootstrap
+    n_bootstraps = boot_loops
+    rng_seed = 42  # control reproducibility
+    bootstrapped_scores = []
+
+    rng = np.random.RandomState(rng_seed)
+    for i in range(n_bootstraps):
+        # bootstrap by sampling with replacement on the prediction indices
+        indices = rng.random_integers(0, n_data - 1, n_data)
+
+        score = calc_DO(fx[indices], gx[indices], target[indices], len(fx[indices]))
+        bootstrapped_scores.append(score)
+        #print("Bootstrap #{} ROC area: {:0.3f}".format(i + 1, score))
+        sorted_scores = np.array(bootstrapped_scores)
+        sorted_scores.sort()
+
+        # Computing the lower and upper bound of the 90% confidence interval
+        # You can change the bounds percentiles to 0.025 and 0.975 to get
+        # a 95% confidence interval instead.
+        confidence_lower = sorted_scores[int(0.025 * len(sorted_scores))]
+        confidence_upper = sorted_scores[int(0.975 * len(sorted_scores))]
+        #print("Confidence interval for the score: [{:0.3f} - {:0.3}]".format(confidence_lower, confidence_upper))
+    DO_mean = np.mean(bootstrapped_scores)
+    DO_var = np.std(bootstrapped_scores)
+    return DO_mean, DO_var
+
+
+''' Main DO function '''
+def DO(fx, gx, target, n_data, stats=False, boot_loops=100):
     if stats==True:
-        n_bootstraps = 1000
-        rng_seed = 42  # control reproducibility
-        bootstrapped_scores = []
-
-        rng = np.random.RandomState(rng_seed)
-        for i in range(n_bootstraps):
-            # bootstrap by sampling with replacement on the prediction indices
-            indices = rng.random_integers(0, n_data - 1, n_data)
-
-            score = calc_DO(fx, gx, target, n_data)
-            bootstrapped_scores.append(score)
-            #print("Bootstrap #{} ROC area: {:0.3f}".format(i + 1, score))
-            sorted_scores = np.array(bootstrapped_scores)
-            sorted_scores.sort()
-
-            # Computing the lower and upper bound of the 90% confidence interval
-            # You can change the bounds percentiles to 0.025 and 0.975 to get
-            # a 95% confidence interval instead.
-            confidence_lower = sorted_scores[int(0.025 * len(sorted_scores))]
-            confidence_upper = sorted_scores[int(0.975 * len(sorted_scores))]
-            #print("Confidence interval for the score: [{:0.3f} - {:0.3}]".format(confidence_lower, confidence_upper))
-
-        DO_mean = np.mean(bootstrapped_scores)
-        DO_var = np.std(bootstrapped_scores)
-        return DO_mean, DO_var
+        return boot_strap(fx, gx, target, n_data, boot_loops)
     else:
         return calc_DO(fx, gx, target, n_data)
 
--- discriminant_ordering/test.py
+++ discriminant_ordering/test.py
@@ -3,10 +3,14 @@
 import numpy as np
 from __init__ import DO
 
-n_data = 100
+n_data = 5000
+n_calc = 500
 x = np.random.rand(n_data)
 y = np.random.rand(n_data)
 targets = np.random.randint(2, size=n_data)
 
-print(DO(fx=x, gx=y, target=targets, n_data=n_data))
-print(DO(fx=x, gx=y, target=targets, n_data=n_data, stats=True))
+''' DO calculated without statistics '''
+print(DO(fx=x, gx=y, target=targets, n_data=n_calc))
+
+''' DO calculated with statistics (i.e. mean and stdev of DO) '''
+print(DO(fx=x, gx=y, target=targets, n_data=n_calc, stats=True))
