01fc10ded46ca5a10e487261b02409567860ac54
Taylor Faucett <tfaucett@uci.edu>
2018-03-27 21:24:25 -0700

    removed unnecessary print statement

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='discriminant_ordering',
-      version='0.1.1',
+      version='0.1.5',
       description='Calculates the DO metric',
       url='http://github.com/tfaucett/discriminant_ordering',
       author='Taylor Faucett',
       author_email='tfaucett@uci.edu',
       license='MIT',
       packages=['discriminant_ordering'],
+      install_requires=['numpy'],
       zip_safe=False)
