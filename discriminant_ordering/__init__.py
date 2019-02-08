4d076954ea1dda8fc891b70da69a9a2d7440417f
Taylor Faucett <taylor@Taylors-iMac.local>
2019-02-08 04:00:04 -0800

    Updated to use new definition

--- discriminant_ordering/__init__.py
+++ discriminant_ordering/__init__.py
@@ -36,9 +36,15 @@ def calc_DO(fx, gx, target, n_data):
     # Filter data into signal and background
     fx0, fx1, gx0, gx1 = filter2D(data[:,0], data[:,1], data[:,2])
 
-    heavi_side = heaviside(np.multiply([(x-y) for x in fx0 for y in fx1 ],[(x-y) for x in gx0 for y in gx1 ]))
+    hside = heaviside(np.multiply([(x-y) for x in fx0 for y in fx1 ],[(x-y) for x in gx0 for y in gx1 ]))
 
-    return 2*(np.abs(((1.0/len(heavi_side)) * np.sum(heavi_side)) - 0.5))
+    heavisum = np.abs(((1.0/len(hside)) * np.sum(hside)))
+    if heavisum < 0.5:
+        heavisum = 1- heavisum
+    else:
+        pass
+
+    return heavisum
 
 def calc_DO_itertools(fx, gx, target, n_data):
     # This is an alternate way of calculating the DO. But testing has shown that it's about half as fast as the numpy approach
@@ -59,33 +65,29 @@ def calc_DO_itertools(fx, gx, target, n_data):
     # Filter data into signal and background
     fx0, fx1, gx0, gx1 = filter2D(data[:,0], data[:,1], data[:,2])
 
-    heavi_side = heaviside(np.multiply(np.diff(list(itertools.product(fx0, fx1))),np.diff(list(itertools.product(gx0, gx1)))))
+    hside = heaviside(np.multiply([(x-y) for x in fx0 for y in fx1 ],[(x-y) for x in gx0 for y in gx1 ]))
 
-    return 2*(np.abs((float(1.0/len(heavi_side)) * np.sum(heavi_side)) - 0.5))
+    heavisum = np.abs(((1.0/len(hside)) * np.sum(hside)))
+    if heavisum < 0.5:
+        heavisum = 1- heavisum
+    else:
+        pass
+
+    return heavisum
 
 def boot_strap(fx, gx, target, n_data, boot_loops):
-    # Calculate uncertainty using bootstrap
     n_bootstraps = boot_loops
-    rng_seed = 42  # control reproducibility
+    rng_seed = 42
     bootstrapped_scores = []
-
     rng = np.random.RandomState(rng_seed)
     for i in range(n_bootstraps):
-        # bootstrap by sampling with replacement on the prediction indices
         indices = rng.random_integers(0, n_data - 1, n_data)
-
         score = calc_DO(fx[indices], gx[indices], target[indices], len(fx[indices]))
         bootstrapped_scores.append(score)
-        #print("Bootstrap #{} ROC area: {:0.3f}".format(i + 1, score))
         sorted_scores = np.array(bootstrapped_scores)
         sorted_scores.sort()
-
-        # Computing the lower and upper bound of the 90% confidence interval
-        # You can change the bounds percentiles to 0.025 and 0.975 to get
-        # a 95% confidence interval instead.
         confidence_lower = sorted_scores[int(0.025 * len(sorted_scores))]
         confidence_upper = sorted_scores[int(0.975 * len(sorted_scores))]
-        #print("Confidence interval for the score: [{:0.3f} - {:0.3}]".format(confidence_lower, confidence_upper))
     DO_mean = np.mean(bootstrapped_scores)
     DO_var = np.std(bootstrapped_scores)
     return DO_mean, DO_var
